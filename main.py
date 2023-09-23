59a1530b00a0d6697f0b4b605645765674e4a093
JeetSolanki23 <77983731+JeetSolanki23@users.noreply.github.com>
2023-09-24 00:17:03 +0530

    Update main.py

--- main.py
+++ main.py
@@ -8,30 +8,5 @@ def index():
     return "hello"
     return render_template('index.html')
 
-@app.route('/generate', methods=['POST'])
-def generate():
-    name = request.form['name']
-    template = Image.open('certificate_template.png')  # Replace with your certificate template image
-
-    # Load the font for the certificate text
-    font = ImageFont.truetype('arial.ttf', 48)  # Replace with your desired font and size
-
-    # Create a draw object
-    draw = ImageDraw.Draw(template)
-
-    # Calculate the position to center the text
-    text_width, text_height = draw.textsize(name, font=font)
-    x = (template.width - text_width) / 2
-    y = (template.height - text_height) / 2
-
-    # Draw the text on the certificate
-    draw.text((x, y), name, fill='black', font=font)
-
-    # Save the generated certificate
-    certificate_path = f'certificates/{name}.png'  # Replace with your desired output path
-    template.save(certificate_path)
-
-    return render_template('certificate.html', name=name, certificate_path=certificate_path)
-
 if __name__ == '__main__':
     app.run(debug=True,host="0.0.0.0")
