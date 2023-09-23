b5d0af3adfe05ea97405581f73cfa6537198d998
JeetSolanki23 <77983731+JeetSolanki23@users.noreply.github.com>
2023-09-24 00:12:46 +0530

    Update main.py

--- main.py
+++ main.py
@@ -34,4 +34,4 @@ def generate():
     return render_template('certificate.html', name=name, certificate_path=certificate_path)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True,host="0.0.0.0")
