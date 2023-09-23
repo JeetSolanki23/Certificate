855c62c468d63928fa332cbabe60376d6c62f379
JeetSolanki23 <77983731+JeetSolanki23@users.noreply.github.com>
2023-09-24 00:24:56 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,66 @@
-from flask import Flask, render_template, request
-#from PIL import Image, ImageDraw, ImageFont
+from flask import Flask, request, jsonify
+
+
+import json
+
+
 
 app = Flask(__name__)
 
-@app.route('/')
-def index():
-    return "hello"
-    return render_template('index.html')
 
-if __name__ == '__main__':
-    app.run(debug=True,host="0.0.0.0")
+
+@app.route('/')
+
+def home():
+
+    return "Namaste"
+
+
+
+@app.route('/predict', methods=["GET","POST"])
+
+def predict():
+
+	
+
+    query = request.json['query'].lower()
+
+    apps_list = request.json['apps_list']
+
+
+
+
+
+    # result = {'response' : 'it\'s done'}
+
+    result = varify(query,apps_list)
+
+ 
+
+
+
+    if result['Category'] != 'open app':
+
+        ans = ReplyBrain(query)
+
+        if "'" not in ans:
+
+            result = "{'Category':'reply','speak': '"+ans+"'}"
+
+        else:
+
+            result = '{"Category":"reply","speak": "'+ans+'"}'
+
+
+
+        return result
+
+        # return jsonify(json.loads(result))
+
+    else:
+
+        return jsonify(result)
+
+if __name__=='__main__':
+
+    app.run(debug=True, host="0.0.0.0")
