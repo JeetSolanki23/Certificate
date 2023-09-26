ef1d9115ddd3288eb6a2d9f53e4c1091df68eeaf
JeetSolanki23 <77983731+JeetSolanki23@users.noreply.github.com>
2023-09-26 10:19:09 +0530

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return "Namaste"
+    #return "Namaste"
     return render_template('index.html')
 
 @app.route('/generate', methods=['POST'])
